ed0aa6d9cbce88b538b94e06ffad0627aeb5d3b2
ERLMKea <erlm@kea.dk>
2022-02-16 11:49:42 +0100

    Initial commit

---
+++ js/setbackground.js
@@ -0,0 +1,22 @@
+const out = function (...str) {console.log(...str)};
+
+out("vi er klar til setbackground");
+
+const bdy = document.querySelector("body");
+out(bdy);
+
+const pbCol = document.querySelector(".pbSetColor");
+out(pbCol);
+
+const inpCol = document.querySelector(".getColor");
+out(inpCol);
+
+pbCol.textContent = "Tryk mig for at ændre farven";
+
+function changeColorBody() {
+  const col = inpCol.value;
+  out(col);
+  bdy.style.backgroundColor = col;
+}
+
+pbCol.addEventListener("click", changeColorBody);

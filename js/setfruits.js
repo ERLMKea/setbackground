da31791b81e416b092b8b99eab8414f146ee7ff7
ERLMKea <erlm@kea.dk>
2022-02-18 10:45:55 +0100

    add fruits

--- js/setfruits.js
+++ js/setfruits.js
@@ -1,5 +1,20 @@
+const out = function (...str) {console.log(...str)};
+
 const arrFruits = ['apple', 'orange', 'banana', 'jordbær'];
 
 const pFruit = document.getElementById('fruits');
+out(pFruit);
+
+function putFruits(item) {
+  pFruit.innerHTML += item + '<br>';
+}
+
+function putFruitsIx(item, index) {
+  //pFruit.innerText += index + ':' + item + '<br>';  får plain text så br tag virker ikke
+  pFruit.innerHTML += index + ':' + item + '<br>';
+}
+
+pFruit.innerHTML= '';
+arrFruits.forEach(putFruitsIx);
 
 
